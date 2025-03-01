d03e07ceac7f985d1b2bc8eb799d71f95959819a
Sam Odum <46067752+SamOdum@users.noreply.github.com>
2025-03-01 12:22:07 +0100

    🔧 chore(pre-commit): remove unnecessary husky.sh script and update lint-staged command to use pnpx instead of npx 🔧 chore(eslint.config.js): update eslint configuration to use the correct plugin import for react and typescript-eslint 🔧 chore(eslint.config.js): update rules to use the correct plugin prefix for typescript-eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,26 @@
 const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
 const typescriptParser = require('@typescript-eslint/parser');
 const globals = require('globals');
-const reactPlugin = require('react');
+const reactPlugin = require('eslint-plugin-react');
 
-module.exports = {
-    languageOptions: {
-        parser: typescriptParser,
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        globals: { ...globals.browser, ...globals.node },
+module.exports = [
+    {
+        languageOptions: {
+            parser: typescriptParser,
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: { ...globals.browser, ...globals.node },
+        },
+        plugins: {
+            '@typescript-eslint': typescriptPlugin,
+            react: reactPlugin,
+        },
+        rules: {
+            'prefer-const': 'error',
+            'react/prop-types': 'off',
+            '@typescript-eslint/no-unused-vars': 'error',
+            '@typescript-eslint/no-explicit-any': 'error',
+        },
+        ignores: ['**/.*', '**/*.config.*'],
     },
-    plugins: {
-        typescript: typescriptPlugin,
-        react: reactPlugin,
-    },
-    rules: {
-        'prefer-const': 'error',
-        'react/prop-types': 'off',
-        '@typescript/no-unused-vars': 'error',
-        '@typescript/no-explicit-any': 'error',
-    },
-    ignores: ['**/.*', '**/*.config.*'],
-};
+];
